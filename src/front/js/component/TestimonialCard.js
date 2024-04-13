e594371fb670d7cc61b5e9633defced0ebeae6c7
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-04-13 21:52:19 +0000

    testimonial-home, some details are missing

--- src/front/js/component/TestimonialCard.js
+++ src/front/js/component/TestimonialCard.js
@@ -13,7 +13,7 @@ const TestimonialCard = () => {
                     <h5 className="card-title">Juancito</h5>
                 </div>
                 <div className="card-body">
-                    <p className='mb-4'>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do </p>
+                    <p className='mb-4'>"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who  </p>
 
                     <div className="d-flex justify-content-between align-items-center">
                         <span className="amarillo">
