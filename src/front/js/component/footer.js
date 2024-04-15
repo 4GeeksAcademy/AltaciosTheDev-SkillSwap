2e601add001a508033247b6c038788e71674d83f
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-15 23:07:57 +0000

    Fixed class for className in TheCard and footer components

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -41,9 +41,9 @@ export const Footer = () => (
 
 			<div className="text-light d-flex flex-column">
 				<h6>Find Us!</h6>
-				<span><i class="fa-brands fa-instagram"></i> username</span>
-				<span><i class="fa-brands fa-facebook"></i> username</span>
-				<span><i class="fa-brands fa-x-twitter"></i> username</span>
+				<span><i className="fa-brands fa-instagram"></i> username</span>
+				<span><i className="fa-brands fa-facebook"></i> username</span>
+				<span><i className="fa-brands fa-x-twitter"></i> username</span>
 			</div>
 
 
