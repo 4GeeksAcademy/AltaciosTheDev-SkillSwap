f7ffd127a3a75584302284f24220879b7e00ec39
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-04-17 23:40:41 +0000

    register and login endpoints

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,15 +1,81 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-from flask import Flask, request, jsonify, url_for, Blueprint
+import os
+from flask import Flask, request, jsonify, url_for, Blueprint, current_app
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from datetime import timedelta
+
+
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
+
+#create a User 👤
+@api.route("/signup", methods=["POST"])
+def signup():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return jsonify({"msg": "User are registered"}), 403
+
+    password_hash = current_app.bcrypt.generate_password_hash(password).decode("utf-8")
+
+    new_user = User(email=email, password=password_hash, is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify(msg="user created successful")
+
+#login a user 🦍
+@api.route("/login", methods=["POST"])
+def login():
+    
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"msg": "User not Found"}), 404
+
+    decrypted_password = current_app.bcrypt.check_password_hash(user.password, password)
+
+    if email != user.email or decrypted_password is False:
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    create_access_token(identity = user.email, expires_delta=timedelta(hours=3))
+    return jsonify(user=user.serialize(), access_token=access_token)
+
+
+@api.route("/private", methods=["GET"])
+@jwt_required()
+def private():
+    email = get_jwt_identity()
+
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return jsonify({"msg": "user not Found 🙁"}), 404
+    dictionary = {
+        "user": user.serialize()
+    }
+    return jsonify(dictionary)
+
+
+
+
+
+
+
 
 
 @api.route('/hello', methods=['POST', 'GET'])
--- src/app.py
+++ src/app.py
@@ -10,6 +10,10 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from api.models import db, User
+import json
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
+from flask_bcrypt import Bcrypt
 
 # from models import Person
 
@@ -18,6 +22,14 @@ static_file_dir = os.path.join(os.path.dirname(
     os.path.realpath(__file__)), '../public/')
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+app.config["JWT_SECRET_KEY"] = os.environ.get('JWT_SECRET')
+jwt = JWTManager(app)
+#Configuración bcrypt
+
+bcrypt = Bcrypt(app)
+app.bcrypt = bcrypt
+#Fin configuración bcrypt
 
 # database condiguration
 db_url = os.getenv("DATABASE_URL")
@@ -66,6 +78,10 @@ def serve_any_other_file(path):
     response = send_from_directory(static_file_dir, path)
     response.cache_control.max_age = 0  # avoid cache memory
     return response
+
+
+
+
 
 
 # this only runs if `$ python src/main.py` is executed
