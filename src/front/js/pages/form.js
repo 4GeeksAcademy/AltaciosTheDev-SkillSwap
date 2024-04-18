109399a811f3308a5a6a87d84d8b7c86bdbc5f2e
WoolMike <150397724+WoolMike@users.noreply.github.com>
2024-04-18 00:33:29 +0000

    Vistas de login y registro completas, tal y como lo pone en excalibur

--- src/front/js/pages/form.js
+++ src/front/js/pages/form.js
@@ -14,51 +14,50 @@ import { Register3 } from "./register3";
 export const Form = () => {
     const [page, setpage] = useState(0);
 
-const TitleRegister = ["General Infomation", "Region", "Interestes Skills"];
+    const TitleRegister = ["General Infomation", "Region", "Interestes Skills"];
 
-const Pageregister = () => {
-    if (page === 0) {
-        return <Register/>
-    } else if (page === 1) {
-        return <Register2/>
-    } else {
-        return <Register3/>
-    }
-};
+    const Pageregister = () => {
+        if (page === 0) {
+            return <Register />
+        } else if (page === 1) {
+            return <Register2 />
+        } else {
+            return <Register3 />
+        }
+    };
     return (
+        <div className="backimage">
+            
+                    <div className="col-4">
+                        <div className="backcard">
+                            <div className="cardregister">
+                            <div>
+                                <h1>{TitleRegister[page]}</h1>
+                            </div>
+                            <div>
+                                {Pageregister()}
+                            </div>
+                            <div className="footer">
+                                <button disabled={page == 0} onClick={() => {
 
-        <div className="container-fluid mb-3">
-            <div className="row">
-                <div className="col-6">
-                    <div className="text-center">
-                        <div>
-                            <h1>{TitleRegister[page]}</h1>
-                        </div>
-                        <div>
-                            {Pageregister()}
-                        </div>
-                        <div className="footer">
-                                <button disabled={page==0} onClick={()=>{
-
-                                    setpage((currpage)=>currpage-1);
+                                    setpage((currpage) => currpage - 1);
                                 }}>Previous</button>
-                                <button className="btn bnt-succes" onClick={()=>{
-                                    if(page===TitleRegister.length-1){
+                                <button className="btn bnt-succes" onClick={() => {
+                                    if (page === TitleRegister.length - 1) {
                                         alert("Register Succes");
-                                    }else{
-                                        setpage((currpage)=>currpage+1);
+                                    } else {
+                                        setpage((currpage) => currpage + 1);
                                     }
-                                }}>{page===TitleRegister.length-1 ? "Submit Register" : "Next"}
+                                }}>{page === TitleRegister.length - 1 ? "Submit Register" : "Next"}
                                 </button>
+                            </div>
                         </div>
+                        </div>
+                        
                     </div>
-                </div>
-                <div className="col-6">
-                <img src={rigoImageUrl} className="marco" alt="..." />
-                </div>
-
-            </div>
+                
         </div>
+
 
     );
 };
