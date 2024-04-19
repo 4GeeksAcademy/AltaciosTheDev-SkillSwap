1e0619d353f300f6a10ec8e4279b5661b3f685fb
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-19 23:16:37 +0000

    individual page profile details creatated

--- src/front/js/component/Single/Main.js
+++ src/front/js/component/Single/Main.js
@@ -1,16 +1,60 @@
 import React from 'react'
 // import singleData from "./skills-mock-data.json.json"
 import { Link, useParams } from "react-router-dom";
+import facePhoto from "../../../img/facePhoto.jpg"
 
 function Main() {
   // const { store, actions } = useContext(Context);
-	const params = useParams();
+  const params = useParams();
 
 
   return (
+
+
     <div className="single-container">
-      <h4 className="single-title">Individual Page {params.id}</h4>
-      
+      <h4 className="single-title">Skill Swapper Information</h4>
+      <div className="container single-data-container">
+        <div className='left-column'>
+          <img className="single-data-image" src={facePhoto} />
+        </div>
+        <div className="right-column">
+          <h4>Enzo Altamirano</h4>
+          <nav>
+            <div className="nav nav-tabs" id="nav-tab" role="tablist">
+              <button className="nav-link-single" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
+              <button className="nav-link-single" id="nav-skills-tab" data-bs-toggle="tab" data-bs-target="#nav-skills" type="button" role="tab" aria-controls="nav-skills" aria-selected="true">Skills</button>
+              <button className="nav-link-single" id="nav-schedule-tab" data-bs-toggle="tab" data-bs-target="#nav-schedule" type="button" role="tab" aria-controls="nav-schedule" aria-selected="true">Schedule</button>
+            </div>
+          </nav>
+          <div className="tab-content" id="nav-tabContent">
+            <div className="tab-pane fade show active profile-tab " id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+              <div className="profile-properties">
+                <p>Country:</p>
+                <p>City:</p>
+                <p>Email:</p>
+                <p>Phone</p>
+                <p>Gender:</p>
+                <p>Bio:</p>
+              </div>
+              <div className="profile-values">
+                <p>Honduras</p>
+                <p>San Pedro Sula</p>
+                <p>enzo.altamirano@gmail.com</p>
+                <p>+504-3355-5344</p>
+                <p>Male</p>
+                <p>Innovative Software Developer and Mechatronics Engineer with a commitment for crafting elegant solutions and enhancing user experiences. Leveraging a solid foundation in both frontend and backend development, combined with 2 years of experience in automotive engineering, hardware systems and electronics.</p>
+              </div>
+            </div>
+            <div className="tab-pane fade skills-tab" id="nav-skills" role="tabpanel" aria-labelledby="nav-skills-tab" tabindex="0">
+              <h4>Skills here</h4>
+            </div>
+            <div className="tab-pane fade schedule-tab" id="nav-schedule" role="tabpanel" aria-labelledby="nav-schedule-tab" tabindex="0">
+              <h4>Calendar here</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
