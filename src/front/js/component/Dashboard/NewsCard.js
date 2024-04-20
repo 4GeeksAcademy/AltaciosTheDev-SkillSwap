f05707b29300d5fc0109cb365a736403317360a1
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-20 02:08:40 +0000

    added comments to the newsCard

--- src/front/js/component/Dashboard/NewsCard.js
+++ src/front/js/component/Dashboard/NewsCard.js
@@ -4,6 +4,8 @@ import {
 } from "react-icons/bs";
 import aitakingover from "../../../img/aitakingover.jpg"
 
+
+
 function NewsCard() {
     return (
         <div className="news">
@@ -11,11 +13,11 @@ function NewsCard() {
                 <h4>Trending</h4>
                 <div className="dashboard-card news-card">
                     <div className="dashboard-card-inner">
-                        <h5>AI is taking over:</h5>
+                        <h5>AI is taking over:</h5>{/* REEMPLAZAR TITULO CON TITULO DE API */}
                         <BsNewspaper className="card_icon" />
                     </div>
-                    <img className="news-image" src={aitakingover} />
-                    <button type="button" className="btn btn-primary">Read more</button>
+                    <img className="news-image" src={aitakingover} /> {/* REEMPLAZAR SRC DE IMAGE CON SRC DE API */}
+                    <button type="button" className="btn btn-primary">Read more</button>{/* REEMPLAZAR LINK CON LINK DE API */}
                 </div>
             </div>
         </div>
