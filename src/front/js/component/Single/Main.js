74305d5c48089d594a167cd8016e290fff0bdf96
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-20 01:15:40 +0000

    skill swapper page, skills tab done

--- src/front/js/component/Single/Main.js
+++ src/front/js/component/Single/Main.js
@@ -24,10 +24,11 @@ function Main() {
               <button className="nav-link-single" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
               <button className="nav-link-single" id="nav-skills-tab" data-bs-toggle="tab" data-bs-target="#nav-skills" type="button" role="tab" aria-controls="nav-skills" aria-selected="true">Skills</button>
               <button className="nav-link-single" id="nav-schedule-tab" data-bs-toggle="tab" data-bs-target="#nav-schedule" type="button" role="tab" aria-controls="nav-schedule" aria-selected="true">Schedule</button>
+              <button className="nav-link-single" id="nav-reviews-tab" data-bs-toggle="tab" data-bs-target="#nav-reviews" type="button" role="tab" aria-controls="nav-reviews" aria-selected="true">Reviews</button>
             </div>
           </nav>
           <div className="tab-content" id="nav-tabContent">
-            <div className="tab-pane fade show active profile-tab " id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+            <div className="tab-pane fade profile-tab " id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
               <div className="profile-properties">
                 <p>Country:</p>
                 <p>City:</p>
@@ -46,10 +47,53 @@ function Main() {
               </div>
             </div>
             <div className="tab-pane fade skills-tab" id="nav-skills" role="tabpanel" aria-labelledby="nav-skills-tab" tabindex="0">
-              <h4>Skills here</h4>
+              <div className="skills">
+                <div className="learning">
+                  <h5 className="learning-title">Learning</h5>
+                  <div className="learning-skills">
+                    <h6>Skills</h6>
+                    <p>Javascript:</p>
+                    <p>Python:</p>
+                    <p>Java:</p>
+                    <p>React:</p>
+                    <p>Data bases:</p>
+                  </div>
+                  <div className="learning-levels">
+                    <h6>Level</h6>
+                    <p>Basic</p>
+                    <p>Intermediate</p>
+                    <p>Advanced</p>
+                    <p>Intermediate</p>
+                    <p>Advanced</p>
+                  </div>
+                </div>
+                <div className='tutoring'>
+                  <h5 className="tutoring-title">Tutoring</h5>
+                  <div className="tutoring-skills">
+                    <h6>Skills</h6>
+                    <p>Javascript:</p>
+                    <p>Python:</p>
+                    <p>Java:</p>
+                    <p>React:</p>
+                    <p>Data bases:</p>
+                  </div>
+                  <div className="tutoring-levels">
+                    <h6>Level</h6>
+                    <p>Basic</p>
+                    <p>Intermediate</p>
+                    <p>Advanced</p>
+                    <p>Intermediate</p>
+                    <p>Advanced</p>
+                  </div>
+                </div>
+              </div>
             </div>
+
             <div className="tab-pane fade schedule-tab" id="nav-schedule" role="tabpanel" aria-labelledby="nav-schedule-tab" tabindex="0">
-              <h4>Calendar here</h4>
+              <h4>Calendar feature in maintenance</h4>
+            </div>
+            <div className="tab-pane fade reviews-tab" id="nav-reviews" role="tabpanel" aria-labelledby="nav-reviews-tab" tabindex="0">
+              <h4>Reviews feature in maintenance</h4>
             </div>
           </div>
         </div>
