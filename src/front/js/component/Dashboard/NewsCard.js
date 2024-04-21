c8895eb234bc3f8475a57776633d6aab5f4bec58
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-04-21 15:29:56 +0000

    news api

--- src/front/js/component/Dashboard/NewsCard.js
+++ src/front/js/component/Dashboard/NewsCard.js
@@ -4,21 +4,34 @@ import {
 } from "react-icons/bs";
 import aitakingover from "../../../img/aitakingover.jpg"
 
-// const newsApi = async () => {
-//     const res = await fetch('')
-//    }
-// useEffect(() => {
-   
-// },[])
+
+
+const newsApi = async () => {
+    const res = await fetch('https://newsapi.org/v2/everything?q=technology&from=2024-03-25&sortBy=publishedAt&apiKey=' + process.env.NEWS_API_KEY)
+
+    const data = await res.json();
+    const randomIndex = Math.floor(Math.random() * data.articles.length);
+
+    setNews(data.articles[randomIndex]) 
+    
+   }
+  
+
 
 function NewsCard() {
+    const [news, setNews] = useState();
+
+    useEffect(() => {
+        newsApi()
+    },[])
+
     return (
         <div className="news">
             <div>
                 <h4>Trending</h4>
                 <div className="dashboard-card news-card">
                     <div className="dashboard-card-inner">
-                        <h5>AI is taking over:</h5>{/* REEMPLAZAR TITULO CON TITULO DE API */}
+                        <h5> {news && news.title}</h5>
                         <BsNewspaper className="card_icon" />
                     </div>
                     <img className="news-image" src={aitakingover} /> {/* REEMPLAZAR SRC DE IMAGE CON SRC DE API */}
