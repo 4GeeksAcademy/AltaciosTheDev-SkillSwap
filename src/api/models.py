711fdcc76b164cd6d091aa556ae1e88634029b7d
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-24 18:05:25 +0000

    updated user model, created category model, created skill model

--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    number = db.Column(db.String(120), nullable=False)
+    gender = db.Column(db.String(80), nullable=False)
+    country = db.Column(db.String(120), nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    bio = db.Column(db.Text, nullable=True)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -19,3 +24,34 @@ class User(db.Model):
             "email": self.email,  
             # do not serialize the password, its a security breach
         }
+    
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120),  unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Category {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,  
+            "name": self.name
+        }
+    
+class Skill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120),  unique=True, nullable=False)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship("Category", backref="skills") #helps with a bi directional relationship in which we don't have to specify another and can access the residents of each planet, on the planet object.
+
+    def __repr__(self):
+        return f'<Skill {self.name}>'
+
+    def serialize(self):
+        return {
+            "id": self.id,  
+            "name": self.name,
+            "category": self.category.name if self.category else None
+        }
+    
