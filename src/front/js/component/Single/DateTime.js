59a16656ee855f1935115dd8fcd655ae0164dd26
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-24 01:13:30 +0000

    sessionDetails object gets created successfulyl when submitting

--- src/front/js/component/Single/DateTime.js
+++ src/front/js/component/Single/DateTime.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Stack, TextField } from '@mui/material'; // Import Stack from Material-UI
 import { LocalizationProvider, DatePicker, TimePicker, DateTimePicker } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3'
@@ -7,96 +7,115 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Unstable_Grid2';
-
-
 
 export default function DateTime() {
-    const [selectedDate, setSelectedDate] = React.useState(null);
-    const [selectedTime, setSelectedTime] = React.useState(null);
+    const [selectedDate, setSelectedDate] = useState(null)
+    const [selectedTime, setSelectedTime] = useState(null)
+    const [selectedSkill, setSelectedSkill] = useState(null)
+    const [sessionDetails, setSessionDetails] = useState({
+        learner_id: "",
+        tutor_id: "",
+        skill_id: null,
+        date: null,
+        time: null
+    });
+
     const minDate = new Date(); // Today's date
     // const minTime = new Date(); // Current time
     // minTime.setHours(8, 0, 0); // Example minimum time: 8:00 AM
     // const maxTime = new Date(); // Current time
     // maxTime.setHours(17, 0, 0); // Example maximum time: 5:00 PM
 
-    console.log({
-        selectedDate: selectedDate && selectedDate.toLocaleDateString(),
-        selectedTime: selectedTime && selectedTime.toLocaleTimeString()
-    })
+    useEffect(() => {
+        setSessionDetails(prevSessionDetails => ({
+            ...prevSessionDetails,
+            skill_id: selectedSkill,
+            date: selectedDate ? selectedDate.toLocaleDateString() : null,
+            time: selectedTime ? selectedTime.toLocaleTimeString() : null
+        }));
+    }, [selectedSkill, selectedDate, selectedTime]);
+
+    function handleSubmit(event) {
+        event.preventDefault()
+
+        if (sessionDetails.skill_id && sessionDetails.date && sessionDetails.time){
+            console.log({
+                msg: "Session request successfully sent",
+                sessionDetails: sessionDetails
+            })
+        }
+    }
 
     return (
         <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <form onSubmit={handleSubmit}>
                 <div className='schedule-container'>
-                <Stack className="choose-pickers" spacing={4} sx={{ width: "250px" }}>
-                    <h5>Choose Date & Time:</h5>
-                    <DatePicker
-                        label="Choose Date:"
-                        renderInput={(params) => <TextField {...params} />}
-                        value={selectedDate}
-                        minDate={minDate}
-                        onChange={(newValue) => {
-                            setSelectedDate(newValue)
-                        }}
-                    />
-                    <TimePicker
-                        label="Choose Time:"
-                        renderInput={(params) => <TextField {...params} />}
-                        value={selectedTime}
-                        // minTime={minTime}
-                        // maxTime={maxTime}
-                        inputFormat="HH:mm"
-                        // ampm={false} // Set to false for 24-hour format
-                        onChange={(newValue) => {
-                            setSelectedTime(newValue)
-                        }}
-                    />
-                </Stack>
-                <Stack className="choose-skills">
-                    <h5>Choose Skill:</h5>
-                    <FormControl>
-                        <FormLabel></FormLabel>
-                        <RadioGroup
-                            name="radio-buttons-group"
-                        >
-                            <FormControlLabel value="javacript" control={<Radio sx={{
-                                color: "#E8E8E8",
-                                '&.Mui-checked': {
-                                    color: "#cf1259",
-                                },
-                            }} />} label="javacript" />
-                            <FormControlLabel value="python" control={<Radio sx={{
-                                color: "#E8E8E8",
-                                '&.Mui-checked': {
-                                    color: "#cf1259",
-                                },
-                            }} />} label="python" />
-                            <FormControlLabel value="java" control={<Radio sx={{
-                                color: "#E8E8E8",
-                                '&.Mui-checked': {
-                                    color: "#cf1259",
-                                },
-                            }} />} label="java" />
-                            <FormControlLabel value="react" control={<Radio sx={{
-                                color: "#E8E8E8",
-                                '&.Mui-checked': {
-                                    color: "#cf1259",
-                                },
-                            }} />} label="react" />
-                            <FormControlLabel value="angular" control={<Radio sx={{
-                                color: "#E8E8E8",
-                                '&.Mui-checked': {
-                                    color: "#cf1259",
-                                },
-                            }} />} label="angular" />
-                        </RadioGroup>
-                    </FormControl>
-                </Stack>
-                <button className="send-session-details">Send Session Details</button>
+                    <Stack className="choose-pickers" spacing={4} sx={{ width: "250px" }}>
+                        <h5>Choose Date & Time:</h5>
+                        <DatePicker
+                            label="Choose Date:"
+                            renderInput={(params) => <TextField {...params} />}
+                            value={selectedDate}
+                            minDate={minDate}
+                            name="date"
+                            onChange={(date) => setSelectedDate(date)}
+                        />
+
+                        <TimePicker
+                            label="Choose Time:"
+                            renderInput={(params) => <TextField {...params} />}
+                            value={selectedTime}
+                            inputFormat="HH:mm"
+                            name="time"
+                            ampm={false}
+                            onChange={(time) => setSelectedTime(time)}
+                        />
+                    </Stack>
+                    <Stack className="choose-skills">
+                        <h5>Choose Skill:</h5>
+                        <FormControl>
+                            <FormLabel></FormLabel>
+                            <RadioGroup
+                                name="skill"
+                                value={selectedSkill}
+                                onChange={(event => setSelectedSkill(event.target.value))}
+                            >
+                                <FormControlLabel value="javacript" control={<Radio sx={{
+                                    color: "#E8E8E8",
+                                    '&.Mui-checked': {
+                                        color: "#cf1259",
+                                    },
+                                }} />} label="javacript" />
+                                <FormControlLabel value="python" control={<Radio sx={{
+                                    color: "#E8E8E8",
+                                    '&.Mui-checked': {
+                                        color: "#cf1259",
+                                    },
+                                }} />} label="python" />
+                                <FormControlLabel value="java" control={<Radio sx={{
+                                    color: "#E8E8E8",
+                                    '&.Mui-checked': {
+                                        color: "#cf1259",
+                                    },
+                                }} />} label="java" />
+                                <FormControlLabel value="react" control={<Radio sx={{
+                                    color: "#E8E8E8",
+                                    '&.Mui-checked': {
+                                        color: "#cf1259",
+                                    },
+                                }} />} label="react" />
+                                <FormControlLabel value="angular" control={<Radio sx={{
+                                    color: "#E8E8E8",
+                                    '&.Mui-checked': {
+                                        color: "#cf1259",
+                                    },
+                                }} />} label="angular" />
+                            </RadioGroup>
+                        </FormControl>
+                    </Stack>
+                    <button type="submit" className="send-session-details">Send Session Details</button>
                 </div>
+            </form>
         </LocalizationProvider>
 
     )
