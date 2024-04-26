2f4d67a28b866d82efb76e656999a3691be778d5
AltaciosTheDev <133562096+AltaciosTheDev@users.noreply.github.com>
2024-04-26 22:06:58 +0000

    implemented filter associations table feature

--- src/front/js/component/Learn/Main.js
+++ src/front/js/component/Learn/Main.js
@@ -1,14 +1,22 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useEffect, useContext, useState } from 'react'
 import { TutorCard } from "../Dashboard/TutorCard";
-import skillsData from "./skills-mock-data.json.json"
 import { Context } from "../../store/appContext";
+
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 
 function Main() {
   const { store, actions } = useContext(Context)
 
+  const [level, setLevel] = useState('');
+  const [role, setRole] = useState('');
+
   useEffect(() => {
-    actions.getAssociations()
+    actions.getAssociations(level, role)
   }, [])
 
   // Declare userSkillElements variable
@@ -32,13 +40,53 @@ function Main() {
   return (
     <div className="learn-container">
       <h4 className="learn-title">What are you looking for?</h4>
-      <div className="learn-input-container">
-        <input className="learn-search" placeholder='Search Student' />
+      <div className="learn-input-container container">
+        {/* <input className="learn-search" placeholder='Search Student' />
         <input className="learn-search" placeholder='Search Tutor' />
         <input className="learn-search" placeholder='Search Skill' />
         <input className="learn-search" placeholder='Search Date' />
         <input className="learn-search" placeholder='Search Time' />
-        <input className="learn-search" placeholder='Search Status' />
+        <input className="learn-search" placeholder='Search Status' /> */}
+
+        <Box sx={{ minWidth: 120, mr: 2 }}>
+          <FormControl fullWidth>
+            <InputLabel id="level-select-label">Level</InputLabel>
+            <Select
+              labelId="level-select-label"
+              id="level-select"
+              value={level}
+              label="Level"
+              onChange={(event) => setLevel(event.target.value)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>              
+              <MenuItem value="Beginner">Beginner</MenuItem>
+              <MenuItem value="Intermediate">Intermediate</MenuItem>
+              <MenuItem value="Advanced">Advanced</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <Box sx={{ minWidth: 120, mr: 2 }}>
+          <FormControl fullWidth>
+            <InputLabel id="role-select-label">Role</InputLabel>
+            <Select
+              labelId="role-select-label"
+              id="role-select"
+              value={role}
+              label="Role"
+              onChange={(event) => setRole(event.target.value)}
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem> 
+              <MenuItem value="Learner">Learner</MenuItem>
+              <MenuItem value="Tutor">Tutor</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+        <button type="submit" className="send-session-details" onClick={() => actions.getAssociations(level, role)}>Search</button>
+
       </div>
       <div className="learn-tutor-cards-container container">
         {userSkillElements}
