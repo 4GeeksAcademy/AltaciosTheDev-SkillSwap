44eca4486ca06166bbffa35707b627915dd12126
Jean Abiad <116680148+Jean-Abiad@users.noreply.github.com>
2024-05-10 22:11:05 +0000

    details changed, colors, backgrounds, footer , dashboard link at navbar deleted, and other details

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -5,60 +5,58 @@ import { useLocation } from "react-router-dom";
 export const Footer = () => {
 
 	const location = useLocation();
-    const excludePaths = ["/dashboard","/learn","/history","/profile","/favorites"]; //insert in this array other paths where navbar is not wanted.
-    const shouldExcludeFooter = excludePaths.includes(location.pathname);
-	const isSinglePage  = location.pathname.startsWith("/single/");
+	const excludePaths = ["/dashboard", "/learn", "/history", "/profile", "/favorites"]; //insert in this array other paths where navbar is not wanted.
+	const shouldExcludeFooter = excludePaths.includes(location.pathname);
+	const isSinglePage = location.pathname.startsWith("/single/");
 
-    if (shouldExcludeFooter || isSinglePage) {
-        return null; // Don't render anything if the current path is included in excludePaths
-    }
+	if (shouldExcludeFooter || isSinglePage) {
+		return null; // Don't render anything if the current path is included in excludePaths
+	}
 
-	return(
-	<footer className="footer bg-dark">
+	return (
+		<footer className="footer borde-top bg-azul-claro">
 
-		<div className="p-5 d-flex justify-content-between">
-			
-			<div className="text-light d-flex flex-column">
-				 	<Link to="/">
-						<button className="bg-dark text-light border-0">Home</button>
-					</Link>
-				 	<Link to="/demo	">
-						<button className="bg-dark text-light border-0">Blog/News</button>
-					</Link>
-				 	<Link to="/demo	">
-						<button className="bg-dark text-light border-0">How it Works?</button>
-					</Link>
-				 	<Link to="/demo	">
-						<button className="bg-dark text-light border-0">Pricing</button>
-					</Link>
-				 	<Link to="/demo	">
-						<button className="bg-dark text-light border-0">Testimonials</button>
-					</Link>
-			</div>		
-			<div className="vertical-line ms-4"></div>
+			<div className="p-5 d-flex justify-content-between">
 
-			<div className="my-auto">
-				<Link to="/demo	">
-					<button className="bg-dark text-light border-0 ">Testimonials</button>
-				</Link>
-				
-				<Link to="/demo	">
-					<button className="bg-dark text-light border-0">Testimonials</button>
-				</Link>
-				<Link to="/demo	">
-					<button className="bg-dark text-light border-0">Testimonials</button>
-				</Link>
+				<div className="text-light d-flex flex-column">
+					
+					<Link to="/">
+						<button className="bg-azul-claro text-light border-0">Home</button>
+					</Link>
+					<Link to="/howitworks	">
+						<button className="bg-azul-claro text-light border-0">How it Works?</button>
+					</Link>
+					<Link to="/pricing	">
+						<button className="bg-azul-claro text-light border-0">Pricing</button>
+					</Link>
+					<Link to="/testimonials	">
+						<button className="bg-azul-claro text-light border-0">Testimonials</button>
+					</Link>
+				</div>
+				<div className="vertical-line ms-4"></div>
+
+				<div className="my-auto">
+					<Link to="/login	">
+						<button className="bg-azul-claro text-light border-0 ">Login</button>
+					</Link>
+
+					<Link to="/form	">
+						<button className="bg-azul-claro text-light border-0">Register</button>
+					</Link>
+					<Link to="/pricing	">
+						<button className="bg-azul-claro text-light border-0">Pricing</button>
+					</Link>
+				</div>
+				<div className="vertical-line ms-4"></div>
+
+				<div className="text-light d-flex flex-column">
+					<h6>Find Us!</h6>
+					<span><i className="fa-brands fa-instagram"></i> SkillSwap</span>
+					<span><i className="fa-brands fa-facebook"></i> SkillSwap</span>
+					<span><i className="fa-brands fa-x-twitter"></i> SkillSwap</span>
+				</div>
 			</div>
-			<div className="vertical-line ms-4"></div>
-
-			<div className="text-light d-flex flex-column">
-				<h6>Find Us!</h6>
-				<span><i className="fa-brands fa-instagram"></i> username</span>
-				<span><i className="fa-brands fa-facebook"></i> username</span>
-				<span><i className="fa-brands fa-x-twitter"></i> username</span>
-			</div>
-		</div>
-	</footer>
+		</footer>
 	)
 
 };
